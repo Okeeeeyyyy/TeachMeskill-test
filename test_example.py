4ff1120aeb59918564ec72f6c00af8f010d0dc1e
Okeeeeyyyy <poznke@gmail.com>
2025-10-28 15:00:48 +0300

    Add files via upload

--- test_example.py
+++ test_example.py
@@ -1,21 +1,12 @@
-import pytest
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+def test_selenium_web(driver_firefox):
+    url = "https://www.selenium.dev/"
+    driver_firefox.get(url)
+    assert driver_firefox.title == "Selenium"
+    assert driver_firefox.current_url == url
 
 
-@pytest.fixture
-def driver():
-    opts = Options()
-    opts.add_argument("--headless=new")
-    opts.add_argument("--window-size=1280,900")
-    driver = webdriver.Chrome(options=opts)
-    yield driver
-
-    driver.quit()
-
-
-def test_selenium_web(driver):
-    url = "https://www.selenium.dev"
+def test_pytest(driver):
+    url = "https://docs.pytest.org/en/stable/"
     driver.get(url)
-    assert driver.title == "Selenium"
+    assert driver.title == "pytest documentation"
     assert driver.current_url == url
